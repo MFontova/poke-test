fc9e22dbdd2ae22d28ff855899674a4484e441f0
Marc Fontova <mfontovat@gmail.com>
2024-02-02 20:39:37 +0100

    files error and template

--- app/about/template.tsx
+++ app/about/template.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function AboutTemplate({children}: {children: React.ReactNode}) {
   return (
-    <div>{children}</div>
+    <div>
+      {children}
+    </div>
   )
 }
